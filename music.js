ba23d7a6dccac74e5bde5f556ce513126133d9a1
Siddharth Saxena <sid@getlatch.com>
2016-10-14 01:14:09 -0400

    added local storage to save JWT token

--- music.js
+++ music.js
@@ -1,4 +1,8 @@
 var xmlHttp = createXmlHttpRequestObject();
+var token ;
+var access_token;
+handleServerResponse_auth();
+
 
 function createXmlHttpRequestObject(){
 	var xmlHttp;
@@ -49,7 +53,12 @@ function handleServerResponse_signup()
 	{
 		var data=xmlHttp.responseText;
 		var jsonResponse = JSON.parse(data);
+		var success = jsonResponse["success"]
 		alert(jsonResponse["msg"]);
+		if (success == true)
+		{
+			open("webpage.html",'_self');
+		}
 	}
 }
 
@@ -81,13 +90,46 @@ function handleServerResponse_auth()
 		var data=xmlHttp.responseText;
 		var jsonResponse = JSON.parse(data);
 		var result = jsonResponse["success"];
+		token = jsonResponse["token"];
+		localStorage.access_token = token;
 		if (result == true)
 		{
-			open("new_account.html");
+			open("info_page.html",'_self');
 		}
 		else if (result == false)
 		{
 			alert(jsonResponse["msg"]);
 		}
+	}
+}
+
+
+function viewMembers()
+{
+if (xmlHttp.readyState == 0 || xmlHttp.readyState== 4 )
+	{
+		var url = "https://musician.herokuapp.com/musicians";
+		xmlHttp.open("GET",url,true);
+		token = localStorage.access_token;
+		xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		xmlHttp.setRequestHeader("Authorization", token);
+		xmlHttp.onreadystatechange = handleServerResponse_findall;
+		xmlHttp.send(null);
+	}
+	else
+	{
+		setTimeout('viewMembers()',1000);
+	}
+}
+
+
+
+function handleServerResponse_findall()
+{
+	if(xmlHttp.readyState == 4 && xmlHttp.status == 200) 
+	{
+		var data=xmlHttp.responseText;
+		var jsonResponse = JSON.parse(data);
+		alert("reached");
 	}
 }
